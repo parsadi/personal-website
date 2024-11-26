0aac94d87d2e94cda592d030f3d5f1a2c5db8167
parsadi <benedictuspurnomo@gmail.com>
2024-11-26 14:08:30 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#text', {
+    strings: ["bilingual", "customer relation", "aviator", "teacher", "learner", "developer on proggress"],
+    typespeed: 30,
+    backspeed: 30,
+    loop: true,
+})
